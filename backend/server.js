1b04b0aecafbc3b05b433c6ce4997ebaf7ab1dd3
ryatsune <carpentieri.yann@gmail.com>
2021-02-09 11:17:48 +0100

    server start-up

---
+++ backend/server.js
@@ -0,0 +1,57 @@
+//import package http de node
+const http = require('http');
+//importe le fichier app.js
+const app = require('./app');
+
+//définit le port sur lequel tourner
+// app.set('port', process.env.PORT || 3000);
+
+const normalizePort = val => {
+    const port = parseInt(val, 10);
+  
+    if (isNaN(port)) {
+      return val;
+    }
+    if (port >= 0) {
+      return port;
+    }
+    return false;
+  };
+  const port = normalizePort(process.env.PORT || '3000');
+  app.set('port', port);
+  
+  const errorHandler = error => {
+    if (error.syscall !== 'listen') {
+      throw error;
+    }
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+    switch (error.code) {
+      case 'EACCES':
+        console.error(bind + ' requires elevated privileges.');
+        process.exit(1);
+        break;
+      case 'EADDRINUSE':
+        console.error(bind + ' is already in use.');
+        process.exit(1);
+        break;
+      default:
+        throw error;
+    }
+};
+
+//créer un serveur avec la méthode createrServeur
+const server = http.createServer(app);
+
+//port à écouter avec la méthode listen
+//variable environement process.env.PORT
+//server.listen(process.env.PORT || 3000);
+
+server.on('error', errorHandler);
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
+});
+
+server.listen(port);
