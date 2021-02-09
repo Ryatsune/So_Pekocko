5033c4e70973d1acfe5262264624f0095acace21
ryatsune <carpentieri.yann@gmail.com>
2021-02-09 11:28:27 +0100

    add CORS middleware

--- backend/app.js
+++ backend/app.js
@@ -4,5 +4,13 @@ const express = require('express');
 //create express application
 const app = express();
 
+//Get route for CORS middleware (security against malicious requests)
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
 //exports app
 module.exports = app;
