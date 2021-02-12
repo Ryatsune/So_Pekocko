74b8a88a6d1e02514a3c6ff46598f8547ecb6d8d
ryatsune <carpentieri.yann@gmail.com>
2021-02-12 12:23:36 +0100

    update sauceSchema

--- backend/models/Sauce.js
+++ backend/models/Sauce.js
@@ -1,17 +1,16 @@
 const mongoose = require('mongoose');
 
 const sauceSchema = mongoose.Schema({
-    _id: { type: String, required: true},
     name: { type: String, required: true },
     manufacturer: { type: String, required: true },
     description: { type: String, required: true },
     heat: { type: Number, required: true },
-    likes: { type: Number, required: false },
-    dislikes: { type: Number, required: false },
+    likes: { type: Number},
+    dislikes: { type: Number},
     imageUrl: { type: String, required: true },
     mainPepper: { type: String, required: true },
-    usersLiked: { type: String, required: false },
-    usersDisliked: { type: String, required: false },
+    usersLiked: { type: Array},
+    usersDisliked: { type: Array},
     userId: { type: String, required: true },
 });
 
