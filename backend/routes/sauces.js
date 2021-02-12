4c4c7b151980f46775eba105939c2a539c85f5eb
ryatsune <carpentieri.yann@gmail.com>
2021-02-12 12:28:00 +0100

    add auth & config-multer middleware

--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -1,24 +1,22 @@
 const express = require('express');
-
-//création d'un routeur avec express
 const router = express.Router();
+
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
 const sauceCtrl = require('../controllers/sauces');
 
-const auth = require('../middleware/auth');
-
+//route POST pour la création d'une nouvelle sauce
+router.post('/', auth, multer, sauceCtrl.createSauce);
 //route GET pour la récupération des sauces
 router.get('/', auth, sauceCtrl.getAllSauces);
 //route GET pour la récupération d'une sauce
 router.get('/:id', auth, sauceCtrl.getOneSauce);
-//route POST pour la création d'une nouvelle sauce
-router.post('/', auth, sauceCtrl.createSauce);
 //route PUT pour la modification d'une sauce existante
-router.put('/:id', auth, sauceCtrl.modifySauce);
+router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 //route DELETE pour la suppression d'une sauce existante
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
 // //route POST pour l'ajout ou suppression de likes
 router.post('/:id/like', auth, sauceCtrl.likeSauce);
-
 
 module.exports = router;
