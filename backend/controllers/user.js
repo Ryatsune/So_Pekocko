7a0571c53f6445d336f2f72cd8664d1c19bb88fa
ryatsune <carpentieri.yann@gmail.com>
2021-03-01 15:12:07 +0100

    add package passwordValidator

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,20 +1,25 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/User');
-
 const jwt = require('jsonwebtoken');
 
+const passwordValidator = require('../middleware/passwordValidator');
+
 exports.signup = (req, res, next) => {
-    bcrypt.hash(req.body.password, 10)
-    .then(hash => {
-        const user = new User({
-            email: req.body.email,
-            password: hash
-        });
-        user.save()
-        .then(() => res.status(201).json({ message: 'Utilisateur crée !' }))
-        .catch(error => res.status(400).json({ error }));
-    })
-    .catch(error => res.status(500).json({ error }));
+    if (!passwordValidator.validate(req.body.password)) {
+        res.status(400).json({ message: 'Mot de passe insuffisant !'})
+    } else {
+        bcrypt.hash(req.body.password, 10)
+        .then(hash => {
+            const user = new User({
+                email: req.body.email,
+                password: hash
+            });
+            user.save()
+            .then(() => res.status(201).json({ message: 'Utilisateur crée !' }))
+            .catch(error => res.status(400).json({ error }));
+        })
+        .catch(error => res.status(500).json({ error }));
+    }
 };
 
 exports.login = (req, res, next) => {
