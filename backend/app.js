5a6c2638136c09f84c260755639c23dfb7229938
ryatsune <carpentieri.yann@gmail.com>
2021-03-01 15:02:36 +0100

    Environment variables for connection to MongoDB

--- backend/app.js
+++ backend/app.js
@@ -7,8 +7,10 @@ const path = require('path');
 const sauceRoutes = require('./routes/sauces');
 const userRoutes = require('./routes/user');
 
+require('dotenv').config();
+
 //Logic to connect to MongoDB
-mongoose.connect('mongodb+srv://Administrator:admin33@sopekocko.fvmie.mongodb.net/<dbname>?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://'+process.env.USER+':'+process.env.PASS+'@'+process.env.URL,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -23,7 +25,7 @@ const app = express();
 
 //Get route for CORS middleware (security against malicious requests)
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', process.env.ORIGIN);
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
